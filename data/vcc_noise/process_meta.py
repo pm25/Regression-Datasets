[fix] fix average score issue.

data/vcc_noise/process_meta.py
@@ -56,33 +56,32 @@ class VCC_NOISE:
             shutil.copy(src_wav, self._audio_folder / src_wav.name)
 
     def _process_split(self, split="train") -> None:
-        split_to_fname = {"train": "TRAINSET", "val": "DEVSET", "test": "TESTSET"}
+        split_to_fname = {"train": "TRAINSET", "eval": "DEVSET", "test": "TESTSET"}
 
         label_csv_path = self._base_folder / "main/DATA/sets" / split_to_fname[split]
         data_df = pd.read_csv(label_csv_path, header=None)
         data_df.columns = ["system", "file_name", "score", "user_id", "info"]
-        grouped = data_df.groupby("file_name")["score"].apply(list).reset_index()
 
+        grouped = data_df.groupby("file_name")["score"].apply(list).reset_index()
         max_scores = grouped["score"].map(len).max()
         scores_expanded = pd.DataFrame(grouped["score"].to_list(), columns=[f"score_{i+1}" for i in range(max_scores)])
+
         meta_df = pd.concat([grouped["file_name"], scores_expanded], axis=1)
         meta_df = meta_df[meta_df["file_name"].apply(lambda x: (self._audio_folder / x).is_file())].reset_index(drop=True)
+        score_cols = [c for c in meta_df.columns if c.startswith("score_")]
+        meta_df["avg_score"] = meta_df[score_cols].mean(axis=1).astype(float)
 
-        meta_df["avg_score"] = scores_expanded.mean(axis=1)
-        meta_df["avg_score"] = meta_df["avg_score"].astype(float)
-        score_cols = [f"score_{i+1}" for i in range(max_scores)]
-        cols_order = ["file_name", "avg_score"] + score_cols
-        meta_df = meta_df[cols_order]
-        return meta_df
+        cols_order = ["file_name", "avg_score"] + [f"score_{i+1}" for i in range(max_scores)]
+        return meta_df[cols_order]
 
     def _process_and_save_meta(self) -> None:
         train_df = self._process_split("train")
-        dev_df = self._process_split("val")
+        eval_df = self._process_split("eval")
         test_df = self._process_split("test")
 
         self._meta_folder.mkdir(parents=True, exist_ok=True)
         train_df.to_csv(self._meta_folder / "train.csv", index=False)
-        dev_df.to_csv(self._meta_folder / "dev.csv", index=False)
+        eval_df.to_csv(self._meta_folder / "eval.csv", index=False)
         test_df.to_csv(self._meta_folder / "test.csv", index=False)
 
 